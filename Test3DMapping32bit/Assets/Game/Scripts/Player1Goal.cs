6be182824ebbfdf2824bc0747726a12bdafe6c46
Bartosz Jędrasik <Bartosz Jdrasik>
2016-03-17 16:15:52 +0100

    Added second player

---
+++ Test3DMapping32bit/Assets/Game/Scripts/Player1Goal.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Player1Goal : MonoBehaviour {
+
+    public Logic Logic;
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        Destroy(collision.gameObject);
+        ++Score.Player2Score;
+        Logic.PuckSpawner.SpawnPuck();
+    }
+}
--- Test3DMapping32bit/Assets/Game/Scripts/Player2ScoreDisplay.cs
+++ Test3DMapping32bit/Assets/Game/Scripts/Player2ScoreDisplay.cs
@@ -16,3 +16,4 @@ public class Player2ScoreDisplay : MonoBehaviour {
 
 
 }
+    
