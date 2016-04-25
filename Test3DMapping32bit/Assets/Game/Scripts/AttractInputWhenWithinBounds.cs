d246a730aa09b8f8e2e0282b9db2528e1298f4da
Renata Ciesielska <reniasac@gmail.com>
2016-04-25 04:58:21 +0200

    added vuforia targets, cube area bounds

---
+++ Test3DMapping32bit/Assets/Game/Scripts/AttractInputWhenWithinBounds.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AttractInputWhenWithinBounds : MonoBehaviour {
+
+    public GameObject Input;
+    public GameObject TableSurface;
+    private float _lampHeight = 1.0f;
+    private float _tableWidth = 1.3f;
+    private float _tableLength = 2.5f;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!(AboveLampHeight() ||
+            LeftOfTableBounds() || RightOfTableBounds() ||
+            BehindTableBounds() || InFrontOfTableBounds()))
+        {
+            if (BelowStandardHeight())
+            {
+                Input.transform.position = new Vector3(transform.position.x, Input.transform.position.y, transform.position.z);
+            }
+            else
+            { 
+                Input.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+            }
+        }
+
+    }
+
+    private bool BehindTableBounds()
+    {
+        return transform.position.z < -_tableLength / 2;
+    }
+
+    private bool InFrontOfTableBounds()
+    {
+        return transform.position.z > _tableLength / 2;
+    }
+
+    private bool LeftOfTableBounds()
+    {
+        return transform.position.x < -_tableWidth / 2;
+    }
+
+    private bool RightOfTableBounds()
+    {
+        return transform.position.x > _tableWidth / 2;
+    }
+
+    private bool AboveLampHeight()
+    {
+        return transform.position.y > TableSurface.transform.position.y + _lampHeight;
+    }
+
+    private bool BelowStandardHeight()
+    {
+        return transform.position.y < TableSurface.transform.position.y + 0.05f;
+    }
+}
---
+++ Test3DMapping32bit/Assets/Game/Scripts/DisallowMovementOutOfTableBounds.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using UnityEngine;
+
+public class DisallowMovementOutOfTableBounds : MonoBehaviour
+{
+    public GameObject TableSurface;
+    private float _lampHeight = 1.0f;
+    private float _tableWidth = 1.3f;
+    private float _tableLength = 2.5f;
+
+
+
+    void Update()
+    {
+        if (BelowStandardHeight())
+        {
+            transform.position = new Vector3(transform.position.x, TableSurface.transform.position.y + 0.05f, transform.position.z);
+        }
+        if (AboveLampHeight())
+        {
+            transform.position = new Vector3(transform.position.x, TableSurface.transform.position.y + _lampHeight, transform.position.z);
+        }
+        if (LeftOfTableBounds())
+        {
+            transform.position = new Vector3(-_tableWidth / 2, transform.position.y, transform.position.z);
+        }
+        if (RightOfTableBounds())
+        {
+            transform.position = new Vector3(_tableWidth / 2, transform.position.y, transform.position.z);
+        }
+        if (InFrontOfTableBounds())
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, _tableLength / 2);
+        }
+        if (BehindTableBounds())
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -_tableLength / 2);
+        }
+    }
+
+    private bool BehindTableBounds()
+    {
+        return transform.position.z < -_tableLength / 2;
+    }
+
+    private bool InFrontOfTableBounds()
+    {
+        return transform.position.z > _tableLength / 2;
+    }
+
+    private bool LeftOfTableBounds()
+    {
+        return transform.position.x < -_tableWidth / 2;
+    }
+
+    private bool RightOfTableBounds()
+    {
+        return transform.position.x > _tableWidth / 2;
+    }
+
+    private bool AboveLampHeight()
+    {
+        return transform.position.y > TableSurface.transform.position.y + _lampHeight;
+    }
+
+    private bool BelowStandardHeight()
+    {
+        return transform.position.y < TableSurface.transform.position.y + 0.05f;
+    }
+}
